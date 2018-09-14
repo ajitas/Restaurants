01f73d0cd832a1467f311b0f21a43eb255409bb1
Ajita Srivastava <ajitas4000@gmail.com>
2018-09-13 20:28:53 -0700

    adding styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 USE restaurants_db;
 
 INSERT INTO restaurants(name) VALUES
-("DishDash"),("Olive Garden"),("Jasmine Thai"),("Super Duper");
+("DishDash"),("Olive Garden"),("Jasmine Thai"),("Super Duper"),("Juban"),("Chipotle"),
+("Panda Express"),("Zahir's Bistro"),("Pho Coung");
 
